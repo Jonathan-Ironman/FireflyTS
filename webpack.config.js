59343525e412493745ce37315ac4454efc24d0d7
Jonathan Ironman <jona84xl@gmail.com>
2018-03-24 11:49:08 +0100

    Added test file

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
-module.exports = {
+module.exports = [{
+    name: "app",
     entry: "./src/main.ts",
     devtool: "inline-source-map",
     output: {
@@ -16,4 +17,22 @@ module.exports = {
             { test: /\.tsx?$/, loader: "ts-loader" }
         ]
     }
-}
+},
+{
+    name: "test",
+    entry: "./src/tests.ts",
+    devtool: "inline-source-map",
+    output: {
+        filename: "test.min.spec.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+        loaders: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+            { test: /\.tsx?$/, loader: "ts-loader" }
+        ]
+    }
+}]
