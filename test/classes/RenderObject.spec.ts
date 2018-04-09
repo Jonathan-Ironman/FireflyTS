9321e7d9fbc0d81738f0b970723ba18657bde1b2
Jonathan Ironman <jona84xl@gmail.com>
2018-04-09 20:07:54 +0200

    Added async test (failing)

--- test/classes/RenderObject.spec.ts
+++ test/classes/RenderObject.spec.ts
@@ -4,18 +4,30 @@ import { IRenderObect } from "../../src/interfaces/IRenderObject";
 
 const imageSrc = "image/objects/test80x100.png";
 
+const objectConfig: IRenderObect = {
+  imageSrc,
+  initialPosition: new Point(10, 20)
+};
+
 describe("RenderObject", () => {
+  afterEach(() => {
+    RenderObject.getObjectList().forEach(o => o.destroy());
+  });
+
+  it("Object should get the correct dimensions once image is loaded", async () => {
+    const renderObject = new RenderObject(objectConfig);
+    await expect(RenderObject.setDimensionsForImage).toBeCalled;
+    await expect(renderObject.width).toBe(80);
+    await expect(renderObject.height).toBe(100);
+  });
+
   it("Add new objects to the object list", () => {
-    const renderObject: IRenderObect = {
-      imageSrc,
-      initialPosition: new Point(10, 20)
-    };
-    const result = new RenderObject(renderObject);
+    const renderObject = new RenderObject(objectConfig);
     expect(RenderObject.getObjectList().length).toBe(1);
   });
 
   it("Remove objects from the object list once destroyed", () => {
-    const renderObject = RenderObject.getObjectList()[0];
+    const renderObject = new RenderObject(objectConfig);
     renderObject.destroy();
     expect(RenderObject.getObjectList().length).toBe(0);
   });
