7e672ba43c8775929637a309c25a5c21394ba1e7
Jonathan Ironman <jona84xl@gmail.com>
2018-04-10 20:50:44 +0200

    Added more tests

--- test/classes/Entity.spec.ts
+++ test/classes/Entity.spec.ts
@@ -1,5 +1,6 @@
 import { Entity } from "../../src/classes/Entity";
 import { Point } from "../../src/classes/Point";
+import { RenderObject } from "../../src/classes/RenderObject";
 
 const imageSrc = "../../public/images/Test80x100.png";
 const defaultPosition = new Point(10, 20);
@@ -18,5 +19,20 @@ describe("Entity", () => {
     const entity = new Entity(imageSrc, defaultPosition);
     entity.destroy();
     expect(Entity.getEntityList().length).toBe(0);
+  });
+
+  test.skip("Should set maxHealth to initial health on first update", () => {
+    const entity = new Entity(imageSrc, defaultPosition);
+    entity.health = 10;
+    entity.update();
+    expect(entity.maxHealth).toBe(10);
+  });
+
+  test("Should call destroy when health drops below 1", () => {
+    const entity = new Entity(imageSrc, defaultPosition);
+    const spy = jest.spyOn(entity, "destroy");
+    entity.health = 0;
+    entity.update();
+    expect(spy).toBeCalled();
   });
 });
